660c86602a0e9f75f5ba404d970d608bad7d9855
Fuad <fuadsanin221.fs@gmail.com>
2022-02-03 16:41:54 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.http import request
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def new_team(request):
+    return render(request,'new_team.html')
+def new_team1(request):
+    return render(request,'new_team1.html')
+
+ 
